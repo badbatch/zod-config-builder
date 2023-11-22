539e91acd235ee0fe670ec132178242e25eea568
Dylan Aubrey <dylan.aubrey@bjss.com>
2023-11-22 13:55:22 +0000

    fix: correct bin path and exports require ext

--- bin/zcb.mjs
+++ bin/zcb.mjs
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
-const { cli } = await import('../dist/main/cli.mjs'); // eslint-disable-line import/no-unresolved
+const { cli } = await import('../dist/esm/cli.mjs'); // eslint-disable-line import/no-unresolved
 cli();
