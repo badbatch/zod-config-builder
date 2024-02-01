d88d412ad334c5a69449815c97d5bd9e8497c483
Dylan Aubrey <dylan.aubrey@bjss.com>
2024-02-01 23:28:47 +0000

    fix: update generic type for clone non enumerable values

--- src/transformers/cloneNonEnumerableValues.ts
+++ src/transformers/cloneNonEnumerableValues.ts
@@ -3,9 +3,12 @@ import { type AnyRecord, NonEmumeralProperties, type TransformConfigHandlerSync 
 const NON_ENUMERABLE_KEYS = new Set(Object.values(NonEmumeralProperties));
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const cloneNonEnumerableValues: TransformConfigHandlerSync = <Config extends AnyRecord>(
-  clone: Config,
-  config: Config
+export const cloneNonEnumerableValues: TransformConfigHandlerSync = <
+  Config1 extends AnyRecord,
+  Config2 extends AnyRecord
+>(
+  clone: Config1,
+  config: Config2
 ) => {
   for (const nonEnumerableKey of NON_ENUMERABLE_KEYS) {
     if (nonEnumerableKey in config) {
